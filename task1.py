8321a4bd5987270675b78ce62318e3af351ce68e
tangamy <tangamy@umich.edu>
2023-04-10 16:38:14 -0400

    finished task 1

--- task1.py
+++ task1.py
@@ -20,10 +20,24 @@ def find_projection(pts2d, pts3d):
     - M: Numpy array of shape (3,4)
 
     """
-    M = None
-    ###########################################################################
-    # TODO: Your code here                                                    #
-    ###########################################################################
+
+    # M = None
+    # ###########################################################################
+    # # TODO: Your code here                                                    #
+    # ###########################################################################
+
+    # make matrix A
+    N = pts2d.shape[0]
+    A = np.zeros((2*N, 12))
+    for i in range(N):
+        X, Y, Z = pts3d[i]
+        u, v = pts2d[i]
+        A[2*i] = [X, Y, Z, 1, 0, 0, 0, 0, -u*X, -u*Y, -u*Z, -u]
+        A[2*i+1] = [0, 0, 0, 0, X, Y, Z, 1, -v*X, -v*Y, -v*Z, -v]
+
+    # use SVD for projection matrix M
+    _, _, VT = np.linalg.svd(A)
+    M = VT[-1].reshape(3, 4)
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -43,10 +57,29 @@ def compute_distance(pts2d, pts3d):
     - float: a average distance you calculated (threshold is 0.01)
 
     """
-    distance = None
+    # distance = None
     ###########################################################################
     # TODO: Your code here                                                    #
     ###########################################################################
+    # get projection matrix M from find_projection
+    M = find_projection(pts2d, pts3d)
+    num_pts = pts2d.shape[0]
+    total_distance = 0
+
+    for i in range(num_pts):
+        # Convert 3D point to homogeneous coords
+        X_i = np.hstack((pts3d[i], 1))
+
+        # project 3D point to 2D
+        P_i = np.dot(M, X_i)
+        P_i /= P_i[2]
+
+        # get distance between projected point and actual 2D point using 2norm
+        distance = np.linalg.norm(P_i[:2] - pts2d[i])
+        total_distance += distance
+
+    # divide by the number of points
+    distance = total_distance / num_pts
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -55,6 +88,10 @@ def compute_distance(pts2d, pts3d):
 if __name__ == '__main__':
     pts2d = np.loadtxt("task1/pts2d.txt")
     pts3d = np.loadtxt("task1/pts3d.txt")
+
+    M = find_projection(pts2d, pts3d)
+    print("M1 is", M)
+
 
     # Alternately, for some of the data, we provide pts1/pts1_3D, which you
     # can check your system on via
--- task23.py
+++ task23.py
@@ -4,6 +4,40 @@ import cv2
 import pdb
 import os
 
+def normalize_points(pts):
+    """
+    Normalizes the points such that they have zero mean and unit standard deviation,
+    and their average distance to the origin is sqrt(2)
+
+    Inputs:
+    - pts: Numpy array of shape (N,2) giving image coordinates
+
+    Returns:
+    - pts_norm: Numpy array of shape (N,2) giving the normalized image coordinates
+    - T: Numpy array of shape (3,3) representing the normalization matrix
+    """
+    # Compute mean and standard deviation
+    mean = np.mean(pts, axis=0)
+    std = np.std(pts, axis=0)
+
+    # Scale points to have unit standard deviation and mean at the origin
+    pts_centered = pts - mean
+    pts_norm = pts_centered / std
+
+    # Compute average distance to origin
+    dist = np.sqrt(np.sum(pts_norm**2, axis=1))
+    avg_dist = np.mean(dist)
+
+    # Compute normalization matrix
+    T = np.array([[1/avg_dist, 0, -mean[0]/avg_dist],
+                  [0, 1/avg_dist, -mean[1]/avg_dist],
+                  [0, 0, 1]])
+
+    # Apply normalization matrix to points
+    pts_norm_homogeneous = np.concatenate([pts_norm, np.ones((pts.shape[0], 1))], axis=1)
+    pts_norm_homogeneous = np.dot(T, pts_norm_homogeneous.T).T[:, :2]
+
+    return pts_norm_homogeneous, T
 
 def find_fundamental_matrix(shape, pts1, pts2):
     """
@@ -29,8 +63,66 @@ def find_fundamental_matrix(shape, pts1, pts2):
     #This will give you an answer you can compare with
     #Your answer should match closely once you've divided by the last entry
     FOpenCV, _ = cv2.findFundamentalMat(pts1, pts2, cv2.FM_8POINT)
+    print("correct answer is", FOpenCV)
+    
+    # Normalize the points
+    # T1 = np.array([[2/shape[1], 0, -1],
+    #                [0, 2/shape[0], -1],
+    #                [0, 0, 1]])
+    # T2 = np.array([[2/shape[1], 0, -1],
+    #                [0, 2/shape[0], -1],
+    #                [0, 0, 1]])
+    # pts1_norm = np.dot(T1, np.vstack((pts1.T, np.ones(pts1.shape[0])))).T[:,:2]
+    # pts2_norm = np.dot(T2, np.vstack((pts2.T, np.ones(pts2.shape[0])))).T[:,:2]
 
-    F = np.eye(3)
+    # # Construct the A matrix for the linear system
+    # A = np.zeros((pts1.shape[0], 9))
+    # for i in range(pts1.shape[0]):
+    #     x, y = pts1_norm[i,:]
+    #     u, v = pts2_norm[i,:]
+    #     A[i,:] = [x*u, y*u, u, x*v, y*v, v, x, y, 1]
+
+    # # Solve the linear system to obtain F
+    # U,S,V = np.linalg.svd(A)
+    # F = V[-1,:].reshape(3,3)
+
+    # # Enforce rank-2 constraint on F
+    # U,S,V = np.linalg.svd(F)
+    # S[-1] = 0
+    # F = np.dot(U, np.dot(np.diag(S), V))
+
+    # # Denormalize F
+    # F = np.dot(np.dot(T2.T, F), T1)
+    
+    
+    # Step 1: normalize the points
+    pts1_norm, T1 = normalize_points(pts1, shape)
+    pts2_norm, T2 = normalize_points(pts2, shape)
+
+    # Step 2: compute the matrix A
+    A = construct_A(pts1_norm, pts2_norm)
+
+    # Step 3: solve for F_init using the eigenvector corresponding to the smallest eigenvalue of A^T A
+    ATA = np.dot(A.T, A)
+    w, V = np.linalg.eig(ATA)
+    w_argsort = np.argsort(w)
+    F_init = V[:, w_argsort[0]].reshape(3, 3)
+
+    # Step 4: enforce rank-2 constraint using SVD
+    U, s, Vt = np.linalg.svd(F_init)
+    s[2] = 0
+    F_rank2 = U @ np.diag(s) @ Vt
+
+    # Step 5: denormalize the fundamental matrix
+    F = T2.T @ F_rank2 @ T1
+
+    F = F / F[-1, -1]
+
+
+
+
+    # F = np.eye(3)
+    print("final answer is", F)
     return F
 
 
@@ -116,21 +208,21 @@ if __name__ == '__main__':
         # compute F
         F = find_fundamental_matrix(shape, pts1, pts2)
         # compute the epipoles
-        e1, e2 = compute_epipoles(F)
-        print(e1, e2)
-        #to get the real coordinates, divide by the last entry
-        print(e1[:2]/e1[-1], e2[:2]/e2[-1])
+        # e1, e2 = compute_epipoles(F)
+        # print(e1, e2)
+        # #to get the real coordinates, divide by the last entry
+        # print(e1[:2]/e1[-1], e2[:2]/e2[-1])
 
-        outname = os.path.join(output, name + "_us.png")
-        # If filename isn't provided or is None, this plt.shows().
-        # If it's provided, it saves it
-        draw_epipolar(img1, img2, F, pts1[::10, :], pts2[::10, :],
-                      epi1=e1, epi2=e2, filename=outname)
+        # outname = os.path.join(output, name + "_us.png")
+        # # If filename isn't provided or is None, this plt.shows().
+        # # If it's provided, it saves it
+        # draw_epipolar(img1, img2, F, pts1[::10, :], pts2[::10, :],
+        #               epi1=e1, epi2=e2, filename=outname)
 
-        if 0:
-            #you can turn this on or off
-            pcd = find_triangulation(K1, K2, F, pts1, pts2)
-            visualize_pcd(pcd,
-                          filename=os.path.join(output, name + "_rec.png"))
+        # if 0:
+        #     #you can turn this on or off
+        #     pcd = find_triangulation(K1, K2, F, pts1, pts2)
+        #     visualize_pcd(pcd,
+        #                   filename=os.path.join(output, name + "_rec.png"))
 
 
